f4dbb5f0242541a17e2aa3217f9c03f116502f82
t0108 <t0108@DESKTOP-0E711QL>
2020-07-03 17:41:31 +0900

    11

--- src/main/java/com/hgr/mini1/controller/LoginController.java
+++ src/main/java/com/hgr/mini1/controller/LoginController.java
@@ -1,4 +1,71 @@
+
 package com.hgr.mini1.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hgr.mini1.model.User;
+import com.hgr.mini1.repository.UserRepository;
+
+import lombok.Data;
+
+@Controller
+@Data
+
 public class LoginController {
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	HttpSession session;
+
+	@PostMapping("/id_check")
+	@ResponseBody
+	public String idCheck(@ModelAttribute User user, Model model) {
+		User result = userRepository.findByEmail(user.getEmail());
+		String msg = "";
+		if (result == null) {
+			msg = "1";
+
+		} else {
+			msg = "0";
+
+		}
+		return msg;
+
+	}
+
+	@GetMapping("/signin")
+	public String signin() {
+		return "signin";
+	}
+
+	
+	@PostMapping("/signup")
+	public String signupPost(@ModelAttribute User user, Model model) {
+		userRepository.save(user);
+		return "signin";
+	}
+
+	@GetMapping("/signout")
+	public String signout() {
+		session.removeAttribute("user_info");
+		return "redirect:/";
+	}
+
+	@PostMapping("/signin")
+	public String signinPost(@ModelAttribute User user) {
+		User dbUser = userRepository.findByEmailAndPwd(user.getEmail(), user.getPwd());
+		if (dbUser != null) {
+			session.setAttribute("user_info", dbUser);
+		}
+		return "redirect:/";
+	}
+
 }
--- src/main/resources/static/js/signIn.js
+++ src/main/resources/static/js/signIn.js
@@ -9,3 +9,39 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+var isPass = false;
+
+$('[name=email]').blur(function() {
+	var email = $(this).val();
+	
+	$.ajax({
+		url: 'id_check',
+		type: 'post',
+		data: { 'email': email },
+		success: function(res) {
+			var msg = '';
+			if(res == '1') {
+				msg = '사용 가능한 이메일입니다.';
+				isPass = true;
+				$('#error_email').css("color", "blue");
+			} else {
+				msg = '중복 이메일입니다.';
+				isPass = false;
+				$('#error_email').css("color", "red");
+			} 
+			$('#error_email').text(msg);
+		}
+	});
+	
+});
+
+$('#signUpBtn').click(function() {
+	if(!isPass) {	
+		alert('이미 사용중인 이메일입니다.');
+		return false;
+	} else {
+		alert('가입을 축하 드립니다.');
+		return true;
+	}
+});
